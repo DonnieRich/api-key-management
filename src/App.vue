b72fce5a9d03b96a0834c679ecbeddcb06a5524b
Donnie <donniepask@gmail.com>
2023-06-08 11:54:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<template>
+    <h2>
+        Impostare correttamente le API key in progetti front-end
+    </h2>
+    <p>
+        Ispezionando il codice di questo sito potete vedere come i valori presenti nel codice JS siano facilmente visibili a
+        tutti gli utenti.
+        Questo è un grave problema di sicurezza quando succede con dati sensibili (es. una API Key che dovrebbe invece
+        essere privata e non disponibile a tutti).
+    </p>
+</template>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const NOT_SO_SECRET_KEY = '123abc';
