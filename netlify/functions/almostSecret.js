47b99f123e19b24955e747f77172589ae18d4bc7
Donnie <donniepask@gmail.com>
2023-06-12 13:04:15 +0200

    Added Almost Secret Netlify function

---
+++ netlify/functions/almostSecret.js
@@ -0,0 +1,8 @@
+exports.handler = async function (event, context) {
+    // your server-side functionality
+    const { SECRET_API_KEY } = process.env
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: SECRET_API_KEY }),
+    };
+};
